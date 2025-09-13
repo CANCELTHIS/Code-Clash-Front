7b3c4a4413113de46c01d543195a71033a3fa8cb
Primebee00 <11primebee00@gmail.com>
2025-09-13 13:25:13 +0300

    Update SOCKET_URL

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -1,7 +1,5 @@
-import { io } from 'socket.io-client';
-
-const SOCKET_URL = 'http://localhost:5000';
-
+import { io } from "socket.io-client";
+const SOCKET_URL = "https://code-clash-back-3.onrender.com/";
 class SocketService {
   constructor() {
     this.socket = null;
@@ -9,15 +7,15 @@ class SocketService {
 
   connect() {
     if (!this.socket) {
-      console.log('Connecting to socket...');
+      console.log("Connecting to socket...");
       this.socket = io(`${SOCKET_URL}/arena`);
-      
-      this.socket.on('connect', () => {
-        console.log('Socket connected:', this.socket.id);
+
+      this.socket.on("connect", () => {
+        console.log("Socket connected:", this.socket.id);
       });
-      
-      this.socket.on('disconnect', () => {
-        console.log('Socket disconnected');
+
+      this.socket.on("disconnect", () => {
+        console.log("Socket disconnected");
       });
     }
     return this.socket;
@@ -32,103 +30,103 @@ class SocketService {
 
   joinMatch(arenaId, userId, matchId) {
     if (this.socket) {
-      this.socket.emit('join_match', { arenaId, userId, matchId });
+      this.socket.emit("join_match", { arenaId, userId, matchId });
     }
   }
 
   updateCode(arenaId, userId, code) {
     if (this.socket) {
-      this.socket.emit('code_update', { arenaId, userId, code });
+      this.socket.emit("code_update", { arenaId, userId, code });
     }
   }
 
   startMatch(arenaId, matchId) {
     if (this.socket) {
-      this.socket.emit('match_start', { arenaId, matchId });
+      this.socket.emit("match_start", { arenaId, matchId });
     }
   }
 
   onCodeUpdate(callback) {
     if (this.socket) {
-      this.socket.on('code_update', callback);
+      this.socket.on("code_update", callback);
     }
   }
 
   onMatchStart(callback) {
     if (this.socket) {
-      this.socket.on('match_start', callback);
+      this.socket.on("match_start", callback);
     }
   }
 
   onUserJoined(callback) {
     if (this.socket) {
-      this.socket.on('user_joined', callback);
+      this.socket.on("user_joined", callback);
     }
   }
 
   joinQueue(userId) {
     if (this.socket) {
-      this.socket.emit('join_queue', { userId });
+      this.socket.emit("join_queue", { userId });
     }
   }
 
   leaveQueue(userId) {
     if (this.socket) {
-      this.socket.emit('leave_queue', { userId });
+      this.socket.emit("leave_queue", { userId });
     }
   }
 
   onMatchFound(callback) {
     if (this.socket) {
-      this.socket.on('match_found', callback);
+      this.socket.on("match_found", callback);
     }
   }
 
   onMatchEnded(callback) {
     if (this.socket) {
-      this.socket.on('match_ended', callback);
+      this.socket.on("match_ended", callback);
     }
   }
 
   onYouWon(callback) {
     if (this.socket) {
-      this.socket.on('you_won', callback);
+      this.socket.on("you_won", callback);
     }
   }
 
   onYouLost(callback) {
     if (this.socket) {
-      this.socket.on('you_lost', callback);
+      this.socket.on("you_lost", callback);
     }
   }
 
   sendMessage(arenaId, userId, username, message) {
     if (this.socket) {
-      this.socket.emit('send_message', {
+      this.socket.emit("send_message", {
         arenaId,
         userId,
         username,
         message,
-        timestamp: new Date()
+        timestamp: new Date(),
       });
     }
   }
 
   startTyping(arenaId, userId, username) {
     if (this.socket) {
-      this.socket.emit('typing_start', { arenaId, userId, username });
+      this.socket.emit("typing_start", { arenaId, userId, username });
     }
   }
 
   stopTyping(arenaId, userId, username) {
     if (this.socket) {
-      this.socket.emit('typing_stop', { arenaId, userId, username });
+      this.socket.emit("typing_stop", { arenaId, userId, username });
     }
   }
 
   onUserTyping(callback) {
     if (this.socket) {
-      this.socket.on('user_typing', callback);
+      this.socket.on("user_typing", callback);
     }
   }
 }
